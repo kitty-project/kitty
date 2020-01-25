3430b3518e5188ecc35815881a581c2d38d5211f
julianjupiter <julianjupiter.io@gmail.com>
2020-01-25 18:54:44 +0800

    Added classes for request parameters.

---
+++ src/main/java/kitty/QueryParam.java
@@ -0,0 +1,21 @@
+package kitty;
+
+public class QueryParam {
+    private final Object value;
+
+    private QueryParam(Object value) {
+        this.value = value;
+    }
+
+    public static QueryParam of(Object param) {
+        return new QueryParam(null);
+    }
+
+    public Integer asInt() {
+        return Integer.valueOf((String) value);
+    }
+
+    public String asString() {
+        return (String) value;
+    }
+}
