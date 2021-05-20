26dcc90f74114a1c37e4ffe173258154aec247b9
Julian Jupiter <julianjupiter.io@gmail.com>
2021-05-21 07:29:22 +0800

    Implement get methos with context handler

--- kitty/src/main/java/com/julianjupiter/kitty/KittyFramework.java
+++ kitty/src/main/java/com/julianjupiter/kitty/KittyFramework.java
@@ -1,11 +1,17 @@
 package com.julianjupiter.kitty;
 
+import com.julianjupiter.kitty.http.server.ContextHandler;
 import com.julianjupiter.kitty.http.server.RequestHandler;
 
 /**
  * @author Julian Jupiter
  */
 final class KittyFramework implements Kitty {
+
+    @Override
+    public Kitty get(String path, ContextHandler handler) {
+        return this;
+    }
 
     @Override
     public Kitty get(String path, RequestHandler handler) {
