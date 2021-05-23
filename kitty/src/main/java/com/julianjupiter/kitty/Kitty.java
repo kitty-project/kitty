b95e4f1928d9722d207d8932a7bb7d4569c24970
Julian Jupiter <julianjupiter.io@gmail.com>
2021-05-24 02:38:03 +0800

    Add methods and generics to RouterProxy; Kitty and its implementation also updated

--- kitty/src/main/java/com/julianjupiter/kitty/Kitty.java
+++ kitty/src/main/java/com/julianjupiter/kitty/Kitty.java
@@ -3,7 +3,7 @@ package com.julianjupiter.kitty;
 /**
  * @author Julian Jupiter
  */
-public sealed interface Kitty extends RouterProxy permits KittyFramework {
+public sealed interface Kitty extends RouterProxy<Kitty> permits KittyFramework {
     static Kitty meow() {
         return new KittyFramework();
     }
--- kitty/src/main/java/com/julianjupiter/kitty/KittyFramework.java
+++ kitty/src/main/java/com/julianjupiter/kitty/KittyFramework.java
@@ -1,13 +1,15 @@
 package com.julianjupiter.kitty;
 
+import com.julianjupiter.kitty.http.message.HttpMethod;
 import com.julianjupiter.kitty.http.server.ContextHandler;
 import com.julianjupiter.kitty.http.server.RequestHandler;
+
+import java.util.Set;
 
 /**
  * @author Julian Jupiter
  */
 final class KittyFramework implements Kitty {
-
     @Override
     public Kitty get(String path, ContextHandler handler) {
         return this;
@@ -26,5 +28,95 @@ final class KittyFramework implements Kitty {
     @Override
     public void run(Runnable runnable) {
         runnable.run();
+    }
+
+    @Override
+    public Kitty any(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty any(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty anyOf(Set<HttpMethod> methods, String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty anyOf(Set<HttpMethod> methods, String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty delete(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty delete(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty head(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty head(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty options(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty options(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty patch(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty patch(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty post(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty post(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty put(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty put(String path, ContextHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty trace(String path, RequestHandler handler) {
+        return this;
+    }
+
+    @Override
+    public Kitty trace(String path, ContextHandler handler) {
+        return this;
     }
 }
