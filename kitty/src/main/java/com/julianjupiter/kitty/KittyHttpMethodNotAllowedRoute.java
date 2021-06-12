7d125a2352da668ad73fc88d2e3e0b6a78aa72a0
Julian Jupiter <julianjupiter.io@gmail.com>
2021-06-12 23:43:54 +0800

    Update route for HTTP error 405

--- kitty/src/main/java/com/julianjupiter/kitty/KittyHttpMethodNotAllowedRoute.java
+++ kitty/src/main/java/com/julianjupiter/kitty/KittyHttpMethodNotAllowedRoute.java
@@ -2,11 +2,22 @@ package com.julianjupiter.kitty;
 
 import com.julianjupiter.kitty.http.message.util.HttpMethod;
 import com.julianjupiter.kitty.http.message.util.HttpStatus;
+import com.julianjupiter.kitty.http.message.util.ResponseHeaderName;
+import com.julianjupiter.kitty.util.Constants;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * @author Julian Jupiter
  */
 final class KittyHttpMethodNotAllowedRoute implements Route {
+    private final HttpMethod[] methods;
+
+    public KittyHttpMethodNotAllowedRoute(HttpMethod[] methods) {
+        this.methods = methods;
+    }
+
     @Override
     public HttpMethod method() {
         return null;
@@ -19,6 +30,15 @@ final class KittyHttpMethodNotAllowedRoute implements Route {
 
     @Override
     public Handler handler() {
-        return ((request, response) -> response.status(HttpStatus.NOT_FOUND));
+        return ((request, response) -> response
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(ResponseHeaderName.ALLOW.toString(), this.allowValue())
+        );
+    }
+
+    private String allowValue() {
+        return Arrays.stream(methods)
+                .map(HttpMethod::name)
+                .collect(Collectors.joining(Constants.Characters.COMMA));
     }
 }
