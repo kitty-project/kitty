51a5c8d1554ec29f05152d52dd67b9fefc43bc9c
Julian Jupiter <julianjupiter.io@gmail.com>
2021-06-12 23:42:10 +0800

    Add comma and use constant for default context api

--- kitty/src/main/java/com/julianjupiter/kitty/util/Constants.java
+++ kitty/src/main/java/com/julianjupiter/kitty/util/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
 
     // String Pool
     public static class Characters {
+        public static final String COMMA = ",";
+        public static final String EMPTY_SPACE = "";
         public static final String FORWARD_SLASH = "/";
 
         private Characters() {
@@ -17,7 +19,7 @@ public class Constants {
     public static class Server {
         public static final String DEFAULT_HOST_ADDRESS = "127.0.0.1";
         public static final int DEFAULT_PORT = 8080;
-        public static final String DEFAULT_CONTEXT_PATH = "/";
+        public static final String DEFAULT_CONTEXT_PATH = Characters.FORWARD_SLASH;
 
         private Server() {
         }
