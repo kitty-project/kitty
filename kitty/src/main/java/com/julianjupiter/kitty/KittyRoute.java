2208be88cbe6f39001d8061169c0e6807e5dcc44
Julian Jupiter <julianjupiter.io@gmail.com>
2021-06-28 23:59:48 +0800

    Clean up code

--- kitty/src/main/java/com/julianjupiter/kitty/KittyRoute.java
+++ kitty/src/main/java/com/julianjupiter/kitty/KittyRoute.java
@@ -6,5 +6,4 @@ import com.julianjupiter.kitty.http.message.util.HttpMethod;
  * @author Julian Jupiter
  */
 final record KittyRoute(HttpMethod method, String path, Handler handler) implements Route {
-    public static String x = "";
 }
